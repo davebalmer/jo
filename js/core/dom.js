4c957a7a2a50064c7706e7033430bb5606d7df8a
Dave Balmer <dbalmerjr@gmail.com>
2010-06-10 14:40:18 -0700

    Sugar to support custom UI tags & class names

--- js/core/dom.js
+++ js/core/dom.js
@@ -121,15 +121,24 @@ joDOM = {
 	toggleCSSClass: function(node, classname) {
 		this.removeCSSClass(node, classname, true);
 	},
-	
+
 	create: function(tag, style) {
 		if (!this.enabled)
 			return null;
 
-		var o = document.createElement(tag);
-		
-		if (style)
-			this.setStyle(o, style);
+		if (typeof tag == "object" && typeof tag.tagName == "string") {
+			// being used to create a container for a joView
+			var o = document.createElement(tag.tagName);
+
+			if (tag.className)
+				this.setStyle(o, tag.className);
+		}
+		else {
+			var o = document.createElement(tag);
+
+			if (style)
+				this.setStyle(o, style);
+		}
 		
 		return o;
 	},
