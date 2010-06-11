0c8dfe124229c6544751408185530c5c75883962
Dave Balmer <dbalmerjr@gmail.com>
2010-06-11 14:05:32 -0700

    New expando control (a.k.a. "Drawer")

---
+++ js/ui/expando.js
@@ -0,0 +1,79 @@
+/**
+	joExpando
+	=========
+	
+	A compound UI element which allows the user to hide/show its contents.
+	The first object passed in becomes the trigger control for the container,
+	and the second becoms the container which expands and contracts. This
+	action is controlled in the CSS by the presence of the "open" class.
+	
+	Use
+	---
+	
+		var x = new joExpando([
+			new joSection("Options"),
+			new joContainer([
+				new joLabel("Label"),
+				new joInput("sample field")
+			]
+		]);
+	
+	Extends
+	-------
+	
+	- joContainer
+	
+	Methods
+	-------
+	
+	- `open()`
+	- `close()`
+	- `toggle()`
+	
+	Events
+	------
+	
+	- `openEvent`
+	- `closeEvent`
+
+*/
+joExpando = function(data) {
+	joContainer.apply(this, arguments);
+};
+joExpando.extend(joContainer, {
+	tagName: "joexpando",
+	
+	draw: function() {
+		joContainer.prototype.draw.apply(this, arguments);
+		this.setToggleEvent();
+	},
+	
+	setToggleEvent: function() {
+		joEvent.on(this.container.childNodes[0], "click", this.toggle, this);
+	},
+	
+	toggle: function() {
+		joDOM.toggleCSSClass(this.container, "open");
+	},
+	
+	open: function() {
+		joDOM.addCSSClass(this.container, "open");
+		this.openEvent.fire();
+	},
+	
+	close: function() {
+		joDOM.removeCSSClass(this.container, "open");
+		this.closeEvent.fire();
+	}
+});
+
+joExpandoTitle = function(data) {
+	joView.apply(this, arguments);
+};
+joExpandoTitle.extend(joView, {
+	tagName: "joexpandotitle",
+	
+	draw: function() {
+		this.container.innerHTML = this.data + "<joicon></joicon>";
+	}
+});
