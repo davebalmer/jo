7e82ff6570276c9c1099d9d83d57cbab07ef0ca9
Dave Balmer <dbalmerjr@gmail.com>
2010-09-16 16:07:17 -0700

    Adding new busy control, still work in progress

--- js/ui/busy.js
+++ js/ui/busy.js
@@ -3,7 +3,16 @@ joBusy = function(data) {
 };
 joBusy.extend(joContainer, {
 	tagName: "jobusy",
-	message: "",
+	
+	draw: function() {
+		this.container.innerHTML = "";
+		for (var i = 0; i < 9; i++)
+			this.container.appendChild(joDom.create("jobusyblock"));
+	},
+	
+	setMessage: function(msg) {
+		this.message = msg || "";
+	},
 	
 	setEvents: function() {
 		return this;
