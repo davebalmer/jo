ec77498696e41a819e16f3297a1bc64e580f5595
Dave Balmer <dbalmerjr@gmail.com>
2010-09-16 16:09:34 -0700

    Adding capture method which allows an observer to become the exclusive responder for a given event.

--- js/core/event.js
+++ js/core/event.js
@@ -23,6 +23,14 @@
 */
 
 joEvent = {
+	eventMap: {
+		"mousedown": "touchstart",
+		"mousemove": "touchmove",
+		"mouseup": "touchend",
+		"mouseout": "touchcancel"
+	},
+	touchy: false,
+	
 	getTarget: function(e) {
 		if (!e)
 			var e = window.event;
@@ -36,14 +44,27 @@ joEvent = {
 
 	on: function(element, event, call, context, data, capture) {
 		if (!call || !element)
-			return;
+			return false;
+			
+		if (this.touchy) {
+			if (this.eventMap[event])
+				event = this.eventMap[event];
+		}
 
 		var element = joDOM.get(element);
 		var call = call;
 		var data = data || "";
 
 		function wrappercall(e) {
-			var target = joEvent.getTarget(e);
+			// support touchy platforms,
+			// might reverse this to turn non-touch into touch
+			if (e.touches && e.touches.length == 1) {
+				var touches = e.touches[0];
+				e.pageX = touches.pageX;
+				e.pageY = touches.pageY;
+				e.screenX = touches.screenX;
+				e.screenY = touches.screenY;
+			}
 			
 			if (context)
 				call.call(context, e, data);
@@ -55,8 +76,14 @@ joEvent = {
 			element.attachEvent("on" + event, wrappercall);
 		else
 			element.addEventListener(event, wrappercall, capture || false);
+			
+		return wrappercall;
 	},
-
+	
+	remove: function(element, event, wrappercall) {
+		element.removeEventListener(event, wrappercall);
+	},
+		
 	stop: function(e) {
 		if (e.stopPropagation)
 			e.stopPropagation();
