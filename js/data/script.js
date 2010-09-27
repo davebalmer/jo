cc0bd96eff11536c36cb32132265eecc52d10e30
Dave Balmer <dbalmerjr@gmail.com>
2010-09-27 11:12:05 -0700

    Fixing callback issues, adding error handling

--- js/data/script.js
+++ js/data/script.js
@@ -4,14 +4,14 @@
 	
 	Script tag loader function which can be used to dynamically load script
 	files or make RESTful calls to many JSON services (provided they have some
-	sort of callback ability).
+	sort of callback ability). This is a low-level utility function.
 	
 	> Need a URL with some examples of this.
 	
 	Calling
 	-------
 
-	`joScript(url, callback, context)`
+	`joScript(url, callback, context, errorcallback, errorcontext)`
 	
 	- url
 	- callback is a function (supports bind, in which case context is optional)
@@ -20,21 +20,43 @@
 	Returns
 	-------
 	
-	Returns a reference to the `script` tag which was created in the DOM to make
-	this request.
-*/
+	Calls your handler method and passes a truthy value if there was an error.
+	
+	Use
+	---
+	
+		joScript("myscript.js", function(error, url) {
+			if (error)
+				console.log("script " + url + " didn't load.");
+		}, this);
 
+*/
 function joScript(url, call, context) {
 	var node = joDOM.create('script');
-	node.onload = handler;
+
+	node.onload = onload;
+	node.onerror = onerror;
 	node.src = url;
 	document.body.appendChild(node);
+
+	function onerror() {
+		handler(true);
+	}
 	
-	function handler() {
-		call.call(context || this, node);
-		
-		// cleanup
+	function onload() {
+		handler(false);
+	}
+	
+	function handler(error) {
+		if (call) {
+			if (context)
+				call.call(context, error, url);
+			else
+				call(error, url);
+		}
+
 		document.body.removeChild(node);
+		node = null;
 	}
 }	
 
