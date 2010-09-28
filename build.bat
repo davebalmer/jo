4f65da4208a6de24902cee5ce6aa5b16e264beb8
Dave Balmer <dbalmerjr@gmail.com>
2010-09-27 17:36:20 -0700

    Yeah, backslashes... batch file still untested.

--- build.bat
+++ build.bat
@@ -1,16 +1,18 @@
 @echo off
-if not exists ./js/core/_jo.js goto wrongdir
+if exists js\core\_jo.js goto start
+goto wrongdir
 
-echo "Building js/jo.js"
+:start
+echo "Building js\jo.js"
 
-cd js/core
-copy log.js + _jo.js + dom.js + event.js + subject.js + time.js + yield.js + cache.js + chain.js + clipboard.js + local.js ../jo_core.js
+cd js\core
+copy log.js + _jo.js + dom.js + event.js + subject.js + time.js + yield.js + cache.js + chain.js + clipboard.js + local.js ..\jo_core.js
 
-cd ../data
-copy datasource.js + database.js + sqldatasource.js + script.js + preference.js ../jo_data.js
+cd ..\data
+copy datasource.js + database.js + sqldatasource.js + script.js + preference.js ..\jo_data.js
 
-cd ../ui
-copy view.js + container.js + control.js + button.js + busy.js + list.js + busy.js + caption.js + card.js + stack.js + scroller.js + divider.js + expando.js + flexrow.js + focus.js + footer.js + gesture.js + group.js + html.js + input.js + label.js + menu.js + passwordinput.js + popup.js + screen.js + shim.js + sound.js + stackscroller.js + tabbar.js + table.js + textarea.js + title.js + toolbar.js ../jo_ui.js
+cd ..\ui
+copy view.js + container.js + control.js + button.js + busy.js + list.js + busy.js + caption.js + card.js + stack.js + scroller.js + divider.js + expando.js + flexrow.js + focus.js + footer.js + gesture.js + group.js + html.js + input.js + label.js + menu.js + passwordinput.js + popup.js + screen.js + shim.js + sound.js + stackscroller.js + tabbar.js + table.js + textarea.js + title.js + toolbar.js ..\jo_ui.js
 
 cd ..
 
@@ -21,11 +23,13 @@ del jo_ui.js
 
 if exists jsmin.exe goto minify
 echo "jsmin not found, skipping minification."
+cd ..
 goto end
 
 :minify
-echo "Minifying js/jo.js -> js/jo_min.js"
+echo "Minifying js\jo.js -> js\jo_min.js"
 jsmin < jo.js > jo_min.js
+cd ..
 goto end
 
 :wrongdir
@@ -33,5 +37,4 @@ echo "OOPS: Run this batch file from the jo source directory, i.e. cd jo first."
 goto end
 
 :end
-cd ..
 echo "Done."
