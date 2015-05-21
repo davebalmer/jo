c3dfe11cf9463fd50a7f324b03c0336665b837f0
Dave Balmer <dbalmerjr@gmail.com>
2015-05-21 15:45:55 -0700

    Spacing

--- js/data/data.js
+++ js/data/data.js
@@ -1,3 +1,4 @@
+
 joData = {
 	union: function() {
 		var o = {};
--- js/ui/menu.js
+++ js/ui/menu.js
@@ -1,24 +1,24 @@
 /**
 	joMenu
 	======
-	
+
 	Simple menu class with optional icons.
-	
+
 	Extends
 	-------
-	
+
 	- joList
-	
+
 	Methods
 	-------
-	
+
 	- `setData(menudata)`
-	
+
 	  See the example below for the format of the menu data.
-	
+
 	Use
 	---
-	
+
 		// simple inline menu; you can always setup the menu items (or change
 		// them) but using the `setData()` method, same as any joView
 		var menu = new joMenu([
@@ -26,7 +26,7 @@
 			{ title: "Frequently Asked Questions", id: "faq" },
 			{ title: "Visit our website", id: "visit", icon: "images/web" }
 		]);
-		
+
 		// simple inline function event handler
 		menu.selectEvent.subscribe(function(id) {
 			switch (id) {
@@ -42,17 +42,17 @@
 				break;
 			}
 		});
-	
+
 	Advanced Use
 	------------
-	
+
 	This could actually be called "more consistent and simple" use. If your menus
 	are static, you could always setup an id-based dispatch delegate which pushes
 	the appropriate card based on the menu `id` selected.
 
 	You could use the `id` in conjunction with view keys you create with joCache.
 	The handler would then be something like:
-	
+
 		menu.selectEvent.subscribe(function(id) {
 			mystack.push(joCache.get(id));
 		});
@@ -72,14 +72,14 @@ joMenu.extend(joList, {
 		else
 			this.selectEvent.fire(index);
 	},
-	
+
 	formatItem: function(item, index) {
 		var o = joDOM.create(this.itemTagName);
-		
+
 		// TODO: not thrilled with this system of finding the
 		// selected item. It's flexible but annoying to code to.
 		o.setAttribute("index", index);
-		
+
 		// quick/dirty
 		if (typeof item === "object") {
 			o.innerHTML = item.title;
@@ -87,11 +87,14 @@ joMenu.extend(joList, {
 				o.style.backgroundImage = "url(" + item.icon + ")";
 				joDOM.addCSSClass(o, "icon");
 			}
+			if (item.className) {
+				o.className = item.className;
+			}
 		}
 		else {
 			o.innerHTML = item;
 		}
-		
+
 		return o;
 	}
 });
